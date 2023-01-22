533e2abf2a2aa4a5c56d9d407909184db78ee319
Adit0507 <adi05072002@gmail.com>
2023-01-22 22:55:01 +0530

    Finished Recent orders componenet

---
+++ components/RecentOrders.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { data } from "../data/data";
+import { FaShoppingBag } from "react-icons/fa";
+
+const RecentOrders = () => {
+  return (
+    <div className="w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-scroll">
+      <h1>Recent Orders</h1>
+      <ul>
+        {data.map((order, id) => (
+          <li
+            className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer"
+            key={id}
+          >
+            <div className="rounded-lg bg-purple-100 p-3">
+              <FaShoppingBag className="text-purple-500" />
+            </div>
+
+            <div className="pl-4">
+              <p className="text-gray-800 font-bold">${order.total}</p>
+              <p className="text-gray-400 text-sm">{order.name.first}</p>
+            </div>
+            <p className='lg:flex md:hidden absolute right-6 text-sm'>{order.date}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecentOrders;
